9fafa7bc44a688dbdf87457fee5b069b4bd6ce34
zadonsk02 <95203714+zadonsk02@users.noreply.github.com>
2022-02-21 19:20:27 +0800

    Add files via upload

---
+++ Add-ADUser-PDQ.ps1
@@ -0,0 +1,29 @@
+﻿function Add-ADUser{
+
+$UserList = Import-Csv -Path 'C:\Temp\NewHires.csv' 
+
+foreach ($User in $UserList) {
+
+     $Attributes = @{
+
+        Enabled = $true
+        ChangePasswordAtLogon = $true
+        Path = "OU=Marketing,DC=adatum,DC=com"
+
+        Name = "$($FirstName) $($LastName)"
+        UserPrincipalName = "$($FirstName).$($LastName)@adatum.com"
+        SamAccountName = "($FirstLetterLower[0]) -join $($LastNameLower)"
+
+        GivenName = $User.First
+        Surname = $User.Last
+
+        Company = $User.Company
+        Department = $User.Department
+        Title = $User.Title
+        AccountPassword = $User.password
+        # AccountPassword = "TotallyFakePassword123" | ConvertTo-SecureString -AsPlainText -Force
+     }
+     
+    # New-ADUser @Attributes
+  }         
+} # function Add-ADUser: https://www.pdq.com/blog/add-users-to-ad-with-powershell/
---
+++ Add-ADUser.ps1
@@ -0,0 +1,34 @@
+﻿function Add-ADUser{
+
+$UserList = Import-Csv -Path 'C:\Temp\NewHires.csv' 
+
+foreach ($User in $UserList) {
+
+     $Attributes = @{
+
+        Enabled = $true
+        ChangePasswordAtLogon = $true
+        Path = "OU=Marketing,DC=adatum,DC=com"
+
+        $FirstName = $User.First
+        $LastName = $User.Last
+        Name = "$($FirstName) $($LastName)"
+
+        UserPrincipalName = "$($FirstName).$($LastName)@adatum.com"
+        $FirstLetter = $FirstName.StartsWith()
+        $LastNameLower = $LastName.ToLower()
+        SamAccountName = "($FirstLetterLower[0]) -join $($LastNameLower)"
+
+        GivenName = $User.First
+        Surname = $User.Last
+
+        Company = $User.Company
+        Department = $User.Department
+        Title = $User.Title
+        AccountPassword = $User.password
+        
+     }
+
+    # New-ADUser @Attributes
+  }         
+} # function Add-ADUser
