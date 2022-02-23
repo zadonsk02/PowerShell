a44a3b17aaf4351c5a9d02d47758ea39bec69f82
Andrew Konigsberg <andrew.a.konigsberg@outlook.com>
2022-02-24 07:16:53 +0800

    20220224 Update2

--- Network_Commands.ps1
+++ Network_Commands.ps1
@@ -2,3 +2,6 @@
 Get-DnsClientCache
 #If bad entires, eg NOT EXIST when the entry should:
 Clear-DnsClientCache
+#View Access Control List
+Get-Acl -Path C:\Temp | Format-Table
+(Get-Acl -Path C:\Temp).access | Format-Table
